53c7ef5f815c3992b687ba3b6680838067c22d04
Valentin Zakharenko <vvzakharenko@gmail.com>
2014-03-18 09:10:44 +0200

    add Dbg project for debugging

---
+++ KnuthMorrisPratt/KnuthMorrisPratt.Dbg/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnuthMorrisPratt.Dbg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var f = new FinderFactory<char>().CreateFrom("aaxaayaaxaaz");
+        }
+    }
+}
