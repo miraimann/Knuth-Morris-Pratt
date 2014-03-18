90e0fa0f1863cf566cbd9bbf73075dbdd39ed4b6
Valentin Zakharenko <vvzakharenko@gmail.com>
2014-03-18 04:51:49 +0200

    introduce IFinder<in T>

---
+++ KnuthMorrisPratt/FindingContracts/IFinder.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace System
+{
+    public interface IFinder<in T>
+    {
+        int FindIn(IEnumerable<T> sequence);
+
+        int FindLastIn(IEnumerable<T> sequence);
+
+        IEnumerable<int> FindAllIn(IEnumerable<T> sequence);
+
+        bool ExistsIn(IEnumerable<T> sequence);
+    }
+}
