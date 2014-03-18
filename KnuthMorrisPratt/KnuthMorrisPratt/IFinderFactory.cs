2979e5cef332d13a8e113216eb27fa63835615c5
Valentin Zakharenko <vvzakharenko@gmail.com>
2014-03-18 04:58:26 +0200

    introduce IFinderFactory<in T>

---
+++ KnuthMorrisPratt/KnuthMorrisPratt/IFinderFactory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KnuthMorrisPratt
+{
+    public interface IFinderFactory<in T>
+    {
+        IFinder<T> CreateFrom(IEnumerable<T> word);
+    }
+}
