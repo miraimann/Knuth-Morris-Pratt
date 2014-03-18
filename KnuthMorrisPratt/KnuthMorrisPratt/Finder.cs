f8c79ab1dad3e955e1f21e26d12b588c2ffdd13a
Valentin Zakharenko <vvzakharenko@gmail.com>
2014-03-18 05:04:37 +0200

    create Finder

---
+++ KnuthMorrisPratt/KnuthMorrisPratt/Finder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KnuthMorrisPratt
+{
+    internal class Finder<T> : IFinder<T>
+    {
+        public int FindIn(IEnumerable<T> sequence)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int FindLastIn(IEnumerable<T> sequence)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<int> FindAllIn(IEnumerable<T> sequence)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ExistsIn(IEnumerable<T> sequence)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
