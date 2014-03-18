b3018505bba2d96db07c0a8c0f053f99ffd66f22
Valentin Zakharenko <vvzakharenko@gmail.com>
2014-03-18 09:11:14 +0200

    implement dfa building

--- KnuthMorrisPratt/KnuthMorrisPratt/Finder.cs
+++ KnuthMorrisPratt/KnuthMorrisPratt/Finder.cs
@@ -1,13 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace KnuthMorrisPratt
 {
     internal class Finder<T> : IFinder<T>
     {
+        private readonly T[] _pattern;
+        private readonly int[,] _dfa;
+
         public Finder(IEnumerable<T> word)
         {
-            throw new NotImplementedException();
+            _pattern = word as T[] ?? word.ToArray();
+            
+            var abc = _pattern.Distinct().ToList();
+
+            _dfa = new int[abc.Count, _pattern.Length]; 
+
+            _dfa[0, 0] = 1;
+
+            for (int x = 0, j = 1; j < _pattern.Length; x = _dfa[abc.IndexOf(_pattern[j++]), x])
+            {
+                for (int c = 0; c < abc.Count; c++)
+                    _dfa[c, j] = _dfa[c, x];
+                _dfa[abc.IndexOf(_pattern[j]), j] = j + 1;
+            }
         }
 
         public int FindIn(IEnumerable<T> sequence)
