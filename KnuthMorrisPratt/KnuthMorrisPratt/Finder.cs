0b17e3ea97ee8ff1106a86d1c5c63700f7863da2
Valentin Zakharenko <vvzakharenko@gmail.com>
2014-03-18 10:11:55 +0200

    move _pattern to local variable to pattern

--- KnuthMorrisPratt/KnuthMorrisPratt/Finder.cs
+++ KnuthMorrisPratt/KnuthMorrisPratt/Finder.cs
@@ -6,27 +6,29 @@ namespace KnuthMorrisPratt
 {
     internal class Finder<T> : IFinder<T>
     {
-        private readonly T[] _pattern;
         private readonly int[,] _dfa;
         private readonly Dictionary<T, int> _abc;
+        private readonly int _finalState;
 
         public Finder(IEnumerable<T> word)
         {
-            _pattern = word as T[] ?? word.ToArray();
+            var pattern = word as T[] ?? word.ToArray();
 
-            _abc = _pattern.Distinct()
-                           .Select((v, i) => new { v, i })
-                           .ToDictionary(o => o.v, o => o.i);
+            _finalState = pattern.Length;
 
-            _dfa = new int[_abc.Count, _pattern.Length]; 
+            _abc = pattern.Distinct()
+                          .Select((v, i) => new { v, i })
+                          .ToDictionary(o => o.v, o => o.i);
+
+            _dfa = new int[_abc.Count, pattern.Length]; 
 
             _dfa[0, 0] = 1;
 
-            for (int i = 0, j = 1; j < _pattern.Length; i = _dfa[_abc[_pattern[j++]], i])
+            for (int i = 0, j = 1; j < pattern.Length; i = _dfa[_abc[pattern[j++]], i])
             {
                 for (int c = 0; c < _abc.Count; c++)
                     _dfa[c, j] = _dfa[c, i];
-                _dfa[_abc[_pattern[j]], j] = j + 1;
+                _dfa[_abc[pattern[j]], j] = j + 1;
             }
         }
 
