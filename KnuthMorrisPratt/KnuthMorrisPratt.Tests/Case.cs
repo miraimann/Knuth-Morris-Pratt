a2906ffbcfe9bff4e8cd62afa3d35d736917b0b9
Valentin Zakharenko <vvzakharenko@gmail.com>
2014-03-19 11:40:57 +0200

    introduce Case<T>

---
+++ KnuthMorrisPratt/KnuthMorrisPratt.Tests/Case.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace KnuthMorrisPratt.Tests
+{
+    public class Case<T>  
+    {
+        public Case(IEnumerable<T> pattern, IEnumerable<T> sequence)
+        {
+            Pattern = pattern;
+            Sequence = sequence;
+        }
+
+        public IEnumerable<T> Pattern { get; private set; }
+
+        public IEnumerable<T> Sequence { get; private set; }
+    }
+}
