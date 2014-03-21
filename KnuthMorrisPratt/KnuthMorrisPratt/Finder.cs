aafd95612b2295c27411792415d1e27a693f7226
Valentin Zakharenko <vvzakharenko@gmail.com>
2014-03-21 09:37:05 +0200

    optimize code

--- KnuthMorrisPratt/KnuthMorrisPratt/Finder.cs
+++ KnuthMorrisPratt/KnuthMorrisPratt/Finder.cs
@@ -7,7 +7,6 @@ namespace KnuthMorrisPratt
     internal class Finder<T> : IFinder<T>
     {
         public const int NotFound = -1;
-        private const int CharacterThatIsMissingInThePattern = -1;
         private const int StartingState = 0;
 
         private readonly int[,] _dfa;
@@ -43,13 +42,13 @@ namespace KnuthMorrisPratt
 
         public int FindIn(IEnumerable<T> sequence)
         {
-            var state = StartingState;
-            foreach (var o in SelectAbcIndexesFrom(sequence)
-                                        .Select((c, i) => new { c, i }))
+            int state = StartingState, i = 0;
+            foreach (var c in sequence)
             {
-                state = GoNext(o.c, state);
+                state = GoNext(c, state);
                 if (state == _finalState)
-                    return o.i - _finalState + 1;
+                    return i - _finalState + 1;
+                ++i;
             }
 
             return NotFound;
@@ -57,14 +56,16 @@ namespace KnuthMorrisPratt
 
         public int FindLastIn(IEnumerable<T> sequence)
         {
-            var rusult = NotFound;
-            var state = StartingState;
-            foreach (var o in SelectAbcIndexesFrom(sequence)
-                                        .Select((c, i) => new { c, i }))
+            int rusult = NotFound,
+                state = StartingState,
+                i = 0;
+
+            foreach (var c in sequence)
             {
-                state = GoNext(o.c, state);
+                state = GoNext(c, state);
                 if (state == _finalState)
-                    rusult = o.i - _finalState + 1;
+                    rusult = i - _finalState + 1;
+                ++i;
             }
 
             return rusult;
@@ -72,47 +73,37 @@ namespace KnuthMorrisPratt
 
         public IEnumerable<int> FindAllIn(IEnumerable<T> sequence)
         {
-            var state = StartingState;
-            foreach (var o in SelectAbcIndexesFrom(sequence)
-                                        .Select((c, i) => new { c, i }))
+            int state = StartingState, i = 0;
+            foreach (var c in sequence)
             {
-                state = GoNext(o.c, state);
+                state = GoNext(c, state);
                 if (state == _finalState)
-                    yield return o.i - _finalState + 1;
+                    yield return i++ - _finalState + 1;
             }
         }
 
         public bool ExistsIn(IEnumerable<T> sequence)
         {
-            using (var e = SelectAbcIndexesFrom(sequence).GetEnumerator())
+            int state = StartingState;
+            foreach (var c in sequence)
             {
-                int state = StartingState;
-                while (e.MoveNext())
-                {
-                    state = GoNext(e.Current, state);
-                    if (state == _finalState) return true;
-                }
-
-                return false;
+                state = GoNext(c, state);
+                if (state == _finalState) return true;     
             }
+
+            return false;
         }
 
-        private int GoNext(int c, int state)
+        private int GoNext(T c, int state)
         {
-            return c == CharacterThatIsMissingInThePattern 
-                      ? StartingState 
-                      : _dfa[c, state];
+            return _abc.ContainsKey(c) ? _dfa[_abc[c], state] 
+                                       : StartingState;
         }
 
         private void Copy(int from, int to)
         {
             for (int c = 0; c < _abc.Count; c++)
                 _dfa[c, to] = _dfa[c, from];
-        }
-
-        private IEnumerable<int> SelectAbcIndexesFrom(IEnumerable<T> sequence)
-        {
-            return sequence.Select(c => _abc.ContainsKey(c) ? _abc[c] : CharacterThatIsMissingInThePattern);
         }
     }
 }
