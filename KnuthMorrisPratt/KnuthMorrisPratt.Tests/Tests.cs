5c75369b7b4540b6946918eb0ccdf4cc8bf47722
Valentin Zakharenko <vvzakharenko@gmail.com>
2014-03-23 18:06:18 +0200

    update SSCases to 2.2.0

--- KnuthMorrisPratt/KnuthMorrisPratt.Tests/Tests.cs
+++ KnuthMorrisPratt/KnuthMorrisPratt.Tests/Tests.cs
@@ -10,28 +10,28 @@ namespace KnuthMorrisPratt.Tests
         private readonly IFinderFactory<char> _finderFactory =
                       new FinderFactory<char>();
 
-        [TestCaseSource(typeof(TestCasesProvider.Exists), "All__TestCases")]
+        [TestCaseSource(typeof(AllCases), "ForExists")]
         public bool ExistsIn_Test(Case<char> @case)
         {
             return _finderFactory.CreateFrom(@case.Pattern)
                                  .ExistsIn(@case.Sequence);   
         }
 
-        [TestCaseSource(typeof(TestCasesProvider.FindFirst), "All__TestCases")]
+        [TestCaseSource(typeof(AllCases), "ForFindFirst")]
         public int FindIn_Test(Case<char> @case)
         {
             return _finderFactory.CreateFrom(@case.Pattern)
                                  .FindIn(@case.Sequence);   
         }
 
-        [TestCaseSource(typeof(TestCasesProvider.FindLast), "All__TestCases")]
+        [TestCaseSource(typeof(AllCases), "ForFindLast")]
         public int FindLastIn_Test(Case<char> @case)
         {
             return _finderFactory.CreateFrom(@case.Pattern)
                                  .FindLastIn(@case.Sequence);   
         }
 
-        [TestCaseSource(typeof(TestCasesProvider.FindAll), "All__TestCases")]
+        [TestCaseSource(typeof(AllCases), "ForFindAll")]
         public IEnumerable<int> FindAllIn_Test(Case<char> @case)
         {
             return _finderFactory.CreateFrom(@case.Pattern)
